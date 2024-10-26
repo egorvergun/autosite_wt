b950c8a7b095a04af904b7decee6eb763b2af22e
egorvergun <vergunegor@gmail.com>
2024-10-26 14:21:22 +0200

    test

--- brabus-app/app/page.js
+++ brabus-app/app/page.js
@@ -12,7 +12,7 @@ const Home = () => {
         <Box sx={homeStyles.textContainer}>
             <Typography variant="h3" sx={homeStyles.headline}>
             On fingertips...<br />
-            It is Brabus auto
+            It is Brabus gondoooooooooooooooooooooooooooooon
             </Typography>
             <Typography variant="h6" sx={homeStyles.subheadline}>
             Because you CAN
