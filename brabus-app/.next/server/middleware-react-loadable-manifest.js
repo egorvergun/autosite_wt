225952e7067a2faa10df6a8f4fb9774e11d9e821
egorvergun <vergunegor@gmail.com>
2024-10-28 15:52:39 +0100

    project

---
+++ brabus-app/.next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST="{\"components\\\\CarCarousel.js -> react-slick\":{\"id\":\"components\\\\CarCarousel.js -> react-slick\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_react-slick_lib_index_js.js\"]}}"
